96ca998c47bab68450b946288b5143717896140d
kathydai6 <136662292+kathydai6@users.noreply.github.com>
2026-06-08 17:32:39 -0500

    Create streamlit_app.py
    copied from my Custom Smoothies SIS app

---
+++ streamit_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+session = get_active_session()
+
+st.title(":cup_with_straw: Customize Your Smoothie!🥤")
+st.write(
+  """Choose the fruits you want in your custom Smoothie!
+  """
+)
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The name on your Smoothie will be: ', name_on_order)
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+
+ingredients_list = st.multiselect(
+  'Choose up to 5 ingredients:',
+  my_dataframe,
+  max_selections=5
+)
+
+if ingredients_list:
+    ingredients_string = ''
+    for fruit_chose in ingredients_list:
+        ingredients_string += fruit_chose + ' '
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
+og_dataset = session.table("smoothies.public.orders")
+editable_df = st.data_editor(og_dataset)
+
+if st.button('Submit Changes'):
+    edited_dataset = session.create_dataframe(editable_df)
+    og_dataset.merge(edited_dataset,
+                     (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID']),
+                     [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})])
+    st.success('Order(s) updated!', icon="✅")
